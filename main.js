0cea3070c1b00d3505abecd24b482323dc4f4efe
Ivan-Brcina <ikibrr@gmail.com>
2023-08-31 00:41:40 +0200

    Second Commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const labels = document.getElementsByClassName("navbar-label");
 const navbarItem = document.getElementsByClassName("navbar-item");
 const arrows = document.getElementsByClassName("arrow-down");
 const burger = document.getElementsByClassName("burger");
-const navbarMobile = document.querySelector(".navbar-menu-mobile");
+const navbarMobile = document.querySelector(".navbar-menu");
 const black = document.querySelector(".black");
 
 console.log(navbarItem);
@@ -10,7 +10,6 @@ console.log(navbarItem);
 for (let i = 0; i < burger.length; i++) {
   burger[i].addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("clicked");
     navbarMobile.classList.toggle("navbar-active");
     black.classList.toggle("navbar-active");
   });
@@ -19,7 +18,6 @@ for (let i = 0; i < burger.length; i++) {
 for (let i = 0; i < labels.length; i++) {
   labels[i].addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("clicked");
     navbarItem[i].classList.toggle("dropdown-active");
   });
 }
